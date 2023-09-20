3c837063f31cd1d669513621957a0ee39480e982
Dave Schreiber <dschreiberjr@gmail.com>
2023-09-20 06:04:57 -0400

    Commit rename regions and markers script. Removes file extensions from regions and markers.

---
+++ shreebz - rename regions and markers - remove file extensions.lua
@@ -0,0 +1,22 @@
+local function removeFileExtension(str)
+    return (str:gsub("%..+$", ""))
+end
+
+local function main()
+    -- Iterate over all the regions in the project and collect indices of regions that need renaming
+    local numMarkers = reaper.CountProjectMarkers(0) -- This includes both markers and regions
+    local regionsToUpdate = {}
+    for i = 0, numMarkers - 1 do
+        local num, isrgn, pos, rgnEnd, name, idx, color = reaper.EnumProjectMarkers3(0, i)
+        -- if isrgn then
+            local name = removeFileExtension(name)
+            reaper.SetProjectMarkerByIndex(0, i, isrgn, pos, rgnEnd, idx, name, color)
+        -- end
+    end
+end
+
+-- Start undo block
+reaper.Undo_BeginBlock()
+main()
+-- End undo block
+reaper.Undo_EndBlock("Remove file extensions from region names", -1)
